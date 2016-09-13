328c5ea0ad58860782de972fde656261ba913b92
Conor Cussell <contact@conorcussell.co.uk>
2016-09-13 21:18:38 +0100

    paginate api

--- src/js/main.js
+++ src/js/main.js
@@ -7,20 +7,33 @@ document.onreadystatechange = function () {
 var App = (function App() {
 
   var gigs = [];
+  var page = 1;
+  var total = 0;
 
-  function init() {
-    getGigs();
-  };
-
-  function getGigs() {
-    fetchJsonp('http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip&page=1', {jsonpCallback: 'jsoncallback',})
+  function getGigs(page) {
+    fetchJsonp('http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip&page=' + page, {jsonpCallback: 'jsoncallback',})
       .then(function(response) {
         return response.json()
       }).then(function(json) {
-        gigs.push(json);
+        total = json.resultsPage.totalEntries;
+        var events = json.resultsPage.results.event;
+        events.map(function(event) {
+          gigs.push(event);
+        });
+        page++;
+        if (page * 50 < total ) {
+          getGigs(page);
+        }
+        console.log(page);
       }).catch(function(ex) {
         console.log('parsing failed', ex)
       })
+  };
+
+  function handleShuffle() {
+    var randomIndex = Math.floor(Math.random()*gigs.length);
+    var gig = gigs.splice(randomIndex, 1);
+    console.log(gig[0].displayName, gigs.length, gigs);
   };
 
   var publicAPI = {
@@ -28,4 +41,12 @@ var App = (function App() {
   };
 
   return publicAPI;
+
+  function init() {
+    var shuffle = document.querySelector('js-shuffle-gigs');
+
+    document.addEventListener('click', handleShuffle);
+
+    getGigs(page);
+  };
 })();
