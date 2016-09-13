09da8127e9e89caecabc1ba8637c01b0fefe32a3
Conor Cussell <contact@conorcussell.co.uk>
2016-09-13 10:05:23 +0100

    add app IIFE

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
-    console.log('wtf');
+    App.init();
   }
 };
+
+var App = (function App() {
+  function init() {
+    getGigs();
+  };
+
+  function getGigs() {
+    fetch('http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip', {mode: 'no-cors'})
+    .then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      console.log('parsed json', json)
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    });
+  };
+
+  var publicAPI = {
+    init: init
+  };
+
+  return publicAPI;
+})();
