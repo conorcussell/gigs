0b3a0a26199ecb57082ea675989b82683773cfde
Conor Cussell <contact@conorcussell.co.uk>
2016-09-15 08:33:02 +0100

    split configs

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,7 +2,6 @@ import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
@@ -25,10 +24,6 @@ export default {
     babel({
       exclude: 'node_modules/**',
     }),
-    replace({
-      exclude: 'node_modules/**',
-      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-    }),
-    (process.env.NODE_ENV === 'production' && uglify()),
+    uglify(),
   ],
 };
