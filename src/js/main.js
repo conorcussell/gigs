081a51c9e6dfa004a894b5e5547e11eb6d320df0
Conor Cussell <contact@conorcussell.co.uk>
2016-09-15 08:16:35 +0100

    change into a module

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,11 @@
+import { getGigs, handleShuffle } from './modules/App.js';
+
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
-    App.init();
+    getGigs(1);
+
+    var shuffle = document.querySelector('.js-shuffle-gigs');
+
+    shuffle.addEventListener('click', handleShuffle);
   }
 };
-
-var App = (function App() {
-
-  var gigs = [];
-  var page = 1;
-  var total = 0;
-
-  function getGigs(page) {
-    fetchJsonp('http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip&page=' + page, {jsonpCallback: 'jsoncallback',})
-      .then(function(response) {
-        return response.json()
-      }).then(function(json) {
-        total = json.resultsPage.totalEntries;
-        var events = json.resultsPage.results.event;
-        events.map(function(event) {
-          gigs.push(event);
-        });
-        page++;
-        if (page * 50 < total ) {
-          getGigs(page);
-        }
-        console.log(page);
-      }).catch(function(ex) {
-        console.log('parsing failed', ex)
-      })
-  };
-
-  function handleShuffle() {
-    var randomIndex = Math.floor(Math.random()*gigs.length);
-    var gig = gigs.splice(randomIndex, 1);
-    console.log(gig[0].displayName, gigs.length, gigs);
-  };
-
-  var publicAPI = {
-    init: init
-  };
-
-  return publicAPI;
-
-  function init() {
-    var shuffle = document.querySelector('js-shuffle-gigs');
-
-    document.addEventListener('click', handleShuffle);
-
-    getGigs(page);
-  };
-})();
---
+++ src/js/modules/App.js
@@ -0,0 +1,33 @@
+import fetchJsonp from 'fetch-jsonp';
+
+var gigs = [];
+var page = 1;
+var total = 0;
+
+export function getGigs(page) {
+  fetchJsonp('http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip&page=' + page, {jsonpCallback: 'jsoncallback',})
+    .then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      total = json.resultsPage.totalEntries;
+      var events = json.resultsPage.results.event;
+      events.map(function(event) {
+        gigs.push(event);
+      });
+      page++;
+      if (page * 50 < total ) {
+        getGigs(page);
+      }
+      console.log(page);
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    })
+}
+
+export function handleShuffle() {
+  var randomIndex = Math.floor(Math.random()*gigs.length);
+  var gig = gigs.splice(randomIndex, 1);
+  console.log(gig[0].displayName, gigs.length, gigs);
+}
+
+export default getGigs;
