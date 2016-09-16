21be1ce5e4982479a18ee36f2fc72ffbb876be31
Conor Cussell <contact@conorcussell.co.uk>
2016-09-16 08:44:29 +0100

    add test

---
+++ test/Utils.spec.js
@@ -0,0 +1,8 @@
+import * as Utils from '../src/js/modules/Utils';
+import { expect } from 'chai';
+
+describe('test', () => {
+  it('should be true', () => {
+    expect(true).to.equal(true);
+  });
+});
