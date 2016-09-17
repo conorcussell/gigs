6b613ccaffce33b5c20ee6bcf6ba29219e4d17b4
Conor Cussell <contact@conorcussell.co.uk>
2016-09-18 00:52:02 +0100

    update gitignore and tests

--- src/js/modules/App.js
+++ src/js/modules/App.js
@@ -1,89 +1,22 @@
 import fetchJsonp from 'fetch-jsonp';
-import { randomIndex, getMonth, distance } from './Utils';
 
-let gigs = [];
-let page = 1;
-let total = 0;
-
-export function getGigs(page) {
-  let date = new Date();
-  date = date.toISOString().split('T')[0];
-
-  fetchJsonp(
-    `http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip&min_date=${date}&max_date=${date}&page=${page}`,
-    {
-      jsonpCallback: 'jsoncallback'
-    }
-  )
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      total = json.resultsPage.totalEntries;
-      const events = json.resultsPage.results.event;
-      events.map((event) => {
-        gigs.push(event);
-      });
-      page++;
-      if (page * 50 < total ) {
-        getGigs(page);
-      } else {
-        geolocate();
-      }
-
-      console.log(gigs);
-    }).catch(function(ex) {
-      console.log('parsing failed', ex);
-    });
+export function url(date, userPosition) {
+  const apiKey = 'afM2GDbBHSRIRxf6';
+  const location = userPosition ? `geo:${userPosition.lat},${userPosition.lng}` : 'clientip';
+  return `http://api.songkick.com/api/3.0/events.json?apikey=${apiKey}&location=${location}&min_date=${date}&max_date=${date}`;
 }
 
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const userPosition = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      console.log(userPosition.lat, userPosition.lng);
-      filterGigs(userPosition, gigs);
-    }, (error) => {
-      console.log('error');
-    });
-  }
+export function filterByDistance(array, distance) {
+  return array.filter(item => item.distance < distance);
 }
 
-function filterGigs(userPosition, gigsToFilter) {
-  console.log(gigs.length);
-  gigs = gigs.filter((gig) => {
-    const gigPosition = {
-      lat: gig.location.lat,
-      lng: gig.location.lng
-    };
-    console.log(distance(gigPosition.lat, gigPosition.lng, userPosition.lat, userPosition.lng));
-    return distance(gigPosition.lat, gigPosition.lng, userPosition.lat, userPosition.lng) <= 50;
+export function getGigs(url, page) {
+  fetchJsonp(`${url}&page=${page}`, {jsonpCallback: 'jsoncallback'})
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json;
+  }).catch(function(ex) {
+    return ex;
   });
-
-  console.log(gigs.length, gigs);
 }
-
-export function handleShuffle() {
-  const gig = gigs.splice(randomIndex(gigs), 1);
-  showGig(gig);
-  console.log(gigs);
-}
-
-function showGig(gig) {
-  const container = document.querySelector('.js-gig');
-  container.textContent = stripDate(gig[0].displayName);
-}
-
-function createGig(gig) {
-  const element = document.createElement('div');
-  element.textContent = '';
-  return element;
-}
-
-function stripDate(string) {
-  return string.slice(0, string.indexOf(` (${getMonth()}`));
-}
-
-export default getGigs;
--- test/App.spec.js
+++ test/App.spec.js
@@ -1,7 +1,36 @@
+import { url, filterByDistance } from '../';
 import { expect } from 'chai';
 
-describe('test', () => {
-  it('doesnt do much', () => {
-    expect(true).to.be(true);
+describe('url', () => {
+  describe('given all arguments', () => {
+    it('returns a url with date and lat/lng', () => {
+      const expected = 'http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=geo:52.3554450,0.1762220&min_date=2016-09-19&max_date=2016-09-19';
+      const actual = url('2016-09-19', { lat: '52.3554450', lng: '0.1762220' });
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe('called with a date', () => {
+    it('returns a url with date and clientip', () => {
+      const expected = 'http://api.songkick.com/api/3.0/events.json?apikey=afM2GDbBHSRIRxf6&location=clientip&min_date=2016-09-19&max_date=2016-09-19';
+      const actual = url('2016-09-19', undefined);
+      expect(actual).to.equal(expected);
+    });
+  });
+});
+
+describe('filterByDistance', () => {
+  describe('with no gigs within the distance', () => {
+    it('returns an empty array', () => {
+      const actual = filterByDistance([{distance: 24}, {distance: 12}, {distance: 400}], 10);
+      expect(actual.length).to.equal(0);
+    });
+  });
+
+  describe('with gigs within the distance', () => {
+    it('returns a filtered array', () => {
+      const actual = filterByDistance([{distance: 8}, {distance: 5}, {distance: 1}, {distance: 12}, {distance: 20}], 10);
+      expect(actual.length).to.equal(3);
+    });
   });
 });
