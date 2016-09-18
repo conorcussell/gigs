78dac37541809d82358307fe922d2c0614171b69
Conor Cussell <contact@conorcussell.co.uk>
2016-09-18 18:52:03 +0100

    change test name

--- test/Utils.spec.js
+++ test/Utils.spec.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 describe('distance', () => {
   const position = { lat: '51.5395158', lng: '-0.1024666' };
   const positionTwo = { lat: '51.5429551', lng: '-0.1491962' };
-  it('returns distance in miles between two positions', () => {
+  it('returns distance in miles between two coordinates', () => {
     expect(distance(position.lat, position.lng, positionTwo.lat, positionTwo.lng)).to.equal(2.0220102726167517);
   });
 });
